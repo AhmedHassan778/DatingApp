967d47d44f9f22153bcb68820fde15c84a5a97ba
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2022-08-12 13:33:14 +0200

    Fixed register bug but replacing binding in home.component.html it was [cancelRegister] it must be (cancelRegister) because it is a function not bind property.

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -4,25 +4,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  registerMode:any = false;
+  registerMode = false;
 
-  constructor() { 
-  }
+  constructor() {}
 
-  ngOnInit(): void {
-
-  }
-
-
+  ngOnInit(): void {}
 
   registerModeToggle() {
     this.registerMode = !this.registerMode;
   }
-  
-  cancelRegisterMode(event: boolean){
-    this.registerMode=event;
+
+  cancelRegisterMode(event: boolean) {
+    this.registerMode = event;
   }
 }
